0afbcb689c2560cc1112819c27ba8ec61f555960
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-25 15:10:22 -0300

    Criação do componente RatingResult e adição de margin-bottom no componente Image

---
+++ src/components/atoms/RatingResult/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./RatingResult.css";
+
+const RatingResult = () => {
+  return (
+    <div className="thank-you__result">
+      <p className="thank-you__result__text">You selected 4 out o 5</p>
+    </div>
+  );
+};
+
+export default RatingResult;
--- src/components/molecules/ThankYouHeader/index.jsx
+++ src/components/molecules/ThankYouHeader/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./ThankYouHeader.css";
 import Image from "../../atoms/Image";
+import RatingResult from "../../atoms/RatingResult";
 
 const ThankYouHeader = () => {
   return (
     <>
       <Image></Image>
+      <RatingResult></RatingResult>
     </>
   );
 };
