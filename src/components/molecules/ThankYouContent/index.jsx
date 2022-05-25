254d91c735151237518640a3e49afabb783759b5
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-25 15:14:29 -0300

    Reutiliação dos componentes Title e Pragraph no componente ThankYouContent

--- src/components/molecules/ThankYouContent/index.jsx
+++ src/components/molecules/ThankYouContent/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 import "./ThankYouContent.css";
+import Title from "../../atoms/Title";
+import Paragraph from "../../atoms/Paragraph";
 
-const ThankYouContent = () => {};
+const ThankYouContent = () => {
+  return (
+    <>
+      <Title>Thank You!</Title>
+      <Paragraph>
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don't hesitate to get in touch!
+      </Paragraph>
+    </>
+  );
+};
 
 export default ThankYouContent;
