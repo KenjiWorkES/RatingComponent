ca8c70502afe27c8bb2b91d44fad10a08193a1bd
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-25 15:03:30 -0300

    Criação do componente Image

--- src/components/atoms/Image/index.jsx
+++ src/components/atoms/Image/index.jsx
@@ -5,7 +5,7 @@ import ImageCell from "../../../../public/assets/illustration-thank-you.svg";
 import "./Image.css";
 
 const Image = () => {
-  return <img src={ImageCell}></img>;
+  return <img className="thank-you__img" src={ImageCell} />;
 };
 
 export default Image;
--- src/components/molecules/ThankYouHeader/index.jsx
+++ src/components/molecules/ThankYouHeader/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 import "./ThankYouHeader.css";
+import Image from "../../atoms/Image";
 
-const ThankYouHeader = () => {};
+const ThankYouHeader = () => {
+  return (
+    <>
+      <Image></Image>
+    </>
+  );
+};
 
 export default ThankYouHeader;
